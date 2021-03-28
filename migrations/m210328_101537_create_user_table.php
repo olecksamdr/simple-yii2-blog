bcc55ec86321064f3b4d8ada33c094c6043ff9f0
Oleksandr <velsashok@gmail.com>
2021-03-28 13:18:53 +0300

    Create user and post tables migrations

---
+++ migrations/m210328_101537_create_user_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m210328_101537_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string(100)
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
---
+++ migrations/m210328_101651_create_post_table.php
@@ -0,0 +1,62 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%post}}`.
+ */
+class m210328_101651_create_post_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%post}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(100)->notNull(),
+            'text' => $this->text()->notNull(),
+            'created_at' => $this->date()->notNull(),
+            'updated_at' => $this->date()->notNull(),
+            'is_published' => $this->boolean()->notNull()->defaultvalue(false),
+            'author_id' => $this->integer()->notNull(),
+        ]);
+
+        // creates index for column `author_id`
+        $this->createIndex(
+            'idx-post-author_id',
+            'post',
+            'author_id'
+        );
+
+        // add foreign key for table `user`
+        $this->addForeignKey(
+            'fk-post-author_id',
+            'post',
+            'author_id',
+            'user',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // drops foreign key for table `user`
+        $this->dropForeignKey(
+            'fk-post-author_id',
+            'post'
+        );
+
+        // drops index for column `author_id`
+        $this->dropIndex(
+            'idx-post-author_id',
+            'post'
+        );
+
+        $this->dropTable('{{%post}}');
+    }
+}
