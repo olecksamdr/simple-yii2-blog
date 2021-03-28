74e62f1900127b62fa4db0d28c47dc9c1adba424
Oleksandr <velsashok@gmail.com>
2021-03-28 13:28:37 +0300

    Generate a Post model

---
+++ models/Post.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $text
+ * @property string $created_at
+ * @property string $updated_at
+ * @property int $is_published
+ * @property int $author_id
+ *
+ * @property User $author
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'text', 'created_at', 'updated_at', 'author_id'], 'required'],
+            [['text'], 'string'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['is_published', 'author_id'], 'integer'],
+            [['title'], 'string', 'max' => 100],
+            [['author_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['author_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'text' => 'Text',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'is_published' => 'Is Published',
+            'author_id' => 'Author ID',
+        ];
+    }
+
+    /**
+     * Gets query for [[Author]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAuthor()
+    {
+        return $this->hasOne(User::className(), ['id' => 'author_id']);
+    }
+}
