1378ae7e3e0379f293141255c2b8668635bbf509
Oleksandr <velsashok@gmail.com>
2021-03-29 22:06:19 +0300

    Add fields to the create user migration

--- migrations/m210328_101537_create_user_table.php
+++ migrations/m210328_101537_create_user_table.php
@@ -14,7 +14,15 @@ class m210328_101537_create_user_table extends Migration
     {
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
-            'username' => $this->string(100)
+            'username' => $this->string()->notNull(),
+
+            // https://www.yiiframework.com/doc/api/2.0/yii-web-identityinterface#getAuthKey()-detail
+            'auth_key' => $this->string(32)->notNull(),
+
+            'password_hash' => $this->string()->notNull(),
+            'email' => $this->string()->notNull()->unique(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
         ]);
     }
 
