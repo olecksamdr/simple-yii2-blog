04d0b201a22e528d96b06c644e4120bb01c81c96
Oleksandr <velsashok@gmail.com>
2021-03-30 15:48:35 +0300

    Truncate long content when displaying posts in list

--- views/post/_posts-list-item.php
+++ views/post/_posts-list-item.php
@@ -3,6 +3,7 @@ use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\helpers\Markdown;
 use yii\helpers\HtmlPurifier;
+use yii\helpers\BaseStringHelper;
 ?>
 
 <li class="pb-2">
@@ -16,7 +17,14 @@ use yii\helpers\HtmlPurifier;
       <?= Yii::$app->formatter->asDate($model->created_at).' '. $model->author->username ?>
   </p>
   <p class="lead">
-    <?= HtmlPurifier::process(Markdown::process($model->text)) ?>
+    <?php
+      $unsafe_html = Markdown::process($model->text); 
+      $safe_html = HtmlPurifier::process($unsafe_html);
+    ?>
+    <?= BaseStringHelper::truncateWords($safe_html, 60, '...', true) ?>
   </p>
+  <a class="btn btn-default" href="<?= Url::toRoute(['post/view', 'id' => $model->id]) ?>">
+    Read more
+  </a>
   <hr>
 </li>
