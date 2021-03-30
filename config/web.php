8a8c715c2c1dcfc7d0244050cc798c21c1eac4d2
Oleksandr <velsashok@gmail.com>
2021-03-30 12:29:33 +0300

    Show posts on the default route

--- config/web.php
+++ config/web.php
@@ -6,6 +6,7 @@ $db = require __DIR__ . '/db.php';
 $config = [
     'id' => 'basic',
     'name' => 'Blog',
+    'defaultRoute' => 'post/index',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
